f29498c80ae7f1208c6b61be84fffb2092933833
Le Duc Thang <19522197@gm.uit.edu.vn>
2023-05-14 23:08:41 +0700

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
+import Logo from "../../Assets/Logo.svg";
+
+
+const Header = () => {
+  return (
+    <nav style={{background: "#FF731D"}}>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="navbar-links-container">
+        <a href="">Trang chủ</a>
+        <a href="">Giới thiệu</a>
+        <a href="">Đánh giá</a>
+        <a href="">Liên hệ</a>
+        <button className="primary-button">Đăng nhập</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+
+function HomePage() {
+  return (
+    <div className="home-container">
+      <Header/>
+      Content
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
